78f8cad3f9bd6344cc77d2fa604245f706a7fcba
margulan-kalykul <m_kalykul@kbtu.kz>
2025-01-15 18:52:01 +0500

    Made the filter widget use checkbox

--- articles/filters.py
+++ articles/filters.py
@@ -1,6 +1,12 @@
 import django_filters
 from .models import Tag
+from django import forms
 
 class ArticleFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(lookup_expr='icontains')
-    tags = django_filters.ModelMultipleChoiceFilter(field_name='tags__name', to_field_name='name', queryset=Tag.objects.all())
+    tags = django_filters.ModelMultipleChoiceFilter(
+        field_name='tags__name', 
+        to_field_name='name', 
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
